daf2dd296e050e54a519bdbca3bb10d8cfcff542
Janvi <jg.gupta.janvi@gmail.com>
2023-09-01 00:06:31 +0530

    added navbar

--- app/src/main/java/com/example/miwoktranslationapp/CategoryAdapter.java
+++ app/src/main/java/com/example/miwoktranslationapp/CategoryAdapter.java
@@ -1,13 +1,19 @@
 package com.example.miwoktranslationapp;
 
+import android.content.Context;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-    public CategoryAdapter(FragmentManager fm) {
+    private Context mContext;
+
+    public CategoryAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -20,6 +26,20 @@ public class CategoryAdapter extends FragmentPagerAdapter {
             return new ColorsFragment();
         } else {
             return new PhrasesFragment();
+        }
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
         }
     }
 
--- app/src/main/java/com/example/miwoktranslationapp/MainActivity.java
+++ app/src/main/java/com/example/miwoktranslationapp/MainActivity.java
@@ -3,19 +3,32 @@ package com.example.miwoktranslationapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
+import android.os.StrictMode;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .build());
+        StrictMode.enableDefaults();
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
+        CategoryAdapter adapter = new CategoryAdapter(this,getSupportFragmentManager());
 
         viewPager.setAdapter(adapter);
+
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
